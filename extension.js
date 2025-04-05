c5d50debbb332bdf7a59d339ca2762638e8530a4
brunomnsilva <brunomnsilva@gmail.com>
2025-04-05 11:36:08 +0100

    Cleanup and code organization

--- extension.js
+++ extension.js
@@ -1,24 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constants
+
+const CONFIGURE_C_COMMAND_ID = 'mingw-c-configuration.configure-c';
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// De/Activation Functions 
 
 /**
- * @param {vscode.ExtensionContext} context
+ * This method is called when the extension is activated.
+ * @param {vscode.ExtensionContext} context The extension context provided by VS Code.
  */
 function activate(context) {
+	
+	console.log('Extension "mingw-c-configuration" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mingw-c-configuration" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mingw-c-configuration.configure-c', function () {
-		// The code you place here will be executed every time your command is executed
+	
+	let disposable = vscode.commands.registerCommand(CONFIGURE_C_COMMAND_ID, async () => {
+		
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from MinGW C Configuration!');
@@ -27,8 +27,20 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+/**
+ * This method is called when the extension is deactivated.
+ */
+function deactivate() {
+	console.log('Extension "mingw-c-configuration" is now deactivated.');
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Extension Logic
+
+// TODO
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Export activate and deactivate functions for VS Code
 
 module.exports = {
 	activate,
