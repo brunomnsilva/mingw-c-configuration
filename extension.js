7cbc68469136c38a364fd1f84a4add2ddbf4a738
brunomnsilva <brunomnsilva@gmail.com>
2025-04-05 11:37:50 +0100

    Ensure a workspace is open

--- extension.js
+++ extension.js
@@ -13,25 +13,30 @@ const CONFIGURE_C_COMMAND_ID = 'mingw-c-configuration.configure-c';
  * @param {vscode.ExtensionContext} context The extension context provided by VS Code.
  */
 function activate(context) {
-	
-	console.log('Extension "mingw-c-configuration" is now active!');
+    
+    console.log('Extension "mingw-c-configuration" is now active!');
 
-	
-	let disposable = vscode.commands.registerCommand(CONFIGURE_C_COMMAND_ID, async () => {
-		
+    
+    let disposable = vscode.commands.registerCommand(CONFIGURE_C_COMMAND_ID, async () => {
+        
+        // Ensure a workspace is open
+        if (!vscode.workspace.workspaceFolders || vscode.workspace.workspaceFolders.length === 0) {
+            vscode.window.showErrorMessage('No workspace folder open. Please open a folder to use this command.');
+            return;
+        }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MinGW C Configuration!');
-	});
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from MinGW C Configuration!');
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 /**
  * This method is called when the extension is deactivated.
  */
 function deactivate() {
-	console.log('Extension "mingw-c-configuration" is now deactivated.');
+    console.log('Extension "mingw-c-configuration" is now deactivated.');
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +48,6 @@ function deactivate() {
 // Export activate and deactivate functions for VS Code
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
